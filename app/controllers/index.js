8fc2f1cdeedc25390b4ca340548c67e96572c398
Michael Bahl <michaelbahl@Michaels-MBP.fritz.box>
2015-03-03 02:16:29 +0100

    Initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,20 @@
+
+var images = ["/images/IMG_0001.jpg",
+			"/images/IMG_0002.jpg",
+			"/images/IMG_0003.jpg",
+			"/images/IMG_0004.jpg",
+			"/images/IMG_0005.jpg",
+			"/images/IMG_0006.jpg",
+			"/images/IMG_0007.jpg",
+			"/images/IMG_0008.jpg",
+			"/images/IMG_0009.jpg",
+			"/images/IMG_0010.jpg"];
+		
+$.ifScollableview.init({
+		parent			: $.pics,
+	    index 			: 4,
+		imageData 		: images,
+});
+
+
+$.index.open();
